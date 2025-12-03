a1b12d8367d71c72ec42c357794120922c76ac40
RahulC4 <101122855+RahulC4@users.noreply.github.com>
2025-12-03 15:21:29 +0530

    Create T 57 b.py

---
+++ T 57 b.py
@@ -0,0 +1,9 @@
+if "generated_ppts" not in st.session_state:
+    st.session_state["generated_ppts"] = []
+
+
+if "generated_ppts" not in st.session_state:
+    st.session_state.generated_ppts = []
+
+if "last_generation_log" not in st.session_state:
+    st.session_state.last_generation_log = None
